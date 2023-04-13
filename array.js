ab3d585bfdd4d98067aa064b55d8f106f360dac7
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-13 17:36:09 +0600

    array last files

---
+++ array.js
@@ -0,0 +1,27 @@
+const cuntryName = 'bangaldesh';
+const age = 52;
+const isIndependent = true;
+const student = {
+    name : 'rohan',
+    roll : 1 ,
+    class : 11 ,
+    id : 121
+}
+const friends = [ 13, 33, 43, 54, 67, 87,21,21];
+
+/* function add (num1, num2){
+    return num1 + num2
+}
+console.log(Array.isArray(friends))
+console.log(typeof add);
+console.log(typeof age);
+console.log(typeof student)
+console.log(friends.includes(14));
+console.log(friends.includes(13));
+console.log(friends.includes(54));
+
+ */
+
+const newFriendsAge = [12, 13, 14, 15]
+const allfriends = newFriendsAge.concat(friends);
+console.log(allfriends)
